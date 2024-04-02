1674f26c1c54aeb96e43812266d98edd07fee0dc
anokhijshah <98619247+anokhijshah@users.noreply.github.com>
2024-04-02 12:06:02 -0500

    Update SendDataSample.py

--- SendDataSample.py
+++ SendDataSample.py
@@ -1,17 +1,3 @@
-# Copyright 2017, Digi International Inc.
-#
-# Permission to use, copy, modify, and/or distribute this software for any
-# purpose with or without fee is hereby granted, provided that the above
-# copyright notice and this permission notice appear in all copies.
-#
-# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-
 from digi.xbee.devices import XBeeDevice
 
 # TODO: Replace with the serial port where your local module is connected to.
@@ -35,7 +21,14 @@ def main():
 
         # Obtain the remote XBee device from the XBee network.
         xbee_network = device.get_network()
-        remote_device = xbee_network.discover_device(REMOTE_NODE_ID)
+        nodes = xbee_network.get_devices()
+        remote_device = None
+
+        for node in nodes:
+            if node.get_node_id() == REMOTE_NODE_ID:
+                remote_device = node
+                break
+
         if remote_device is None:
             print("Could not find the remote device")
             exit(1)

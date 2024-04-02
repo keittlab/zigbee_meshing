672d9f17803a63db1acc63212097a071050ed625
anokhijshah <98619247+anokhijshah@users.noreply.github.com>
2024-04-02 12:27:32 -0500

    Update SendDataSample.py

--- SendDataSample.py
+++ SendDataSample.py
@@ -59,16 +59,18 @@ def main():
         nodes = xnet.get_devices()
     
         # Print the output log discovered local nodes.
+        remote_device = None
         print("Discovered local nodes:")
         for node in nodes:
-            print(" - %s" % node)
+            remote_device = node
+            print(" - %s" % remote_device)
 
-        remote_device = xnet.discover_device(REMOTE_NODE_ID)
+        # remote_device = xnet.discover_device(REMOTE_NODE_ID)
         if remote_device is None:
             print("Could not find the remote device")
             exit(1)
 
-        print("Sending data to %s >> %s..." % (remote_device.get_64bit_addr(), DATA_TO_SEND))
+        print("Sending data to %s >> %s..." % (node.get_64bit_addr(), DATA_TO_SEND))
 
         device.send_data(remote_device, DATA_TO_SEND)
 
